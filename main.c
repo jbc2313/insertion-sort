b53785694726c13e63c16318dcd67c039b51537c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-06 12:00:54 -0600

    added list array function

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main() {
 
+
+
+
+void listArray(int *pntr, int size) {
+    int x; 
+    for (x = 0; x < size; x++) {
+        printf("array[%d]=%d\n", x, pntr[x]);
+    };
+
+};
+
+int main() {
+    int arr[] = { 3, 1, 7, 4, 9, 2, 8, 5 };
 
 
     return 0;
