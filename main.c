220fa28569425269b9b7f1a7f631fea0529b85f4
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-06 12:12:55 -0600

    made array size 10, added a duplicate number as well

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 
 
 
-
-
 void listArray(int *pntr, int size) {
     int x; 
     for (x = 0; x < size; x++) {
@@ -16,15 +14,30 @@ void listArray(int *pntr, int size) {
 void insertion(int *pntr, int size) {
    int x, tmp, z;
    for ( x = 1; x < size; x++) {
+        tmp = pntr[x];
+        z = x - 1;
 
-   }
+        while ( z >= 0 && pntr[z] > tmp) {
+            pntr[z + 1] = pntr[z];
+            z = z - 1;
+        };
 
-}
+        pntr[z + 1] = tmp;
+   };
+
+};
 
 
 int main() {
-    int arr[] = { 3, 1, 7, 4, 9, 2, 8, 5 };
-
-
+    int arr[] = { 3, 1, 7, 4, 9, 15, 2, 8, 5, 7 };
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+    listArray(arr, arrSize);
+    insertion(arr, arrSize); 
+    printf("Array was sorted!\n");
+    listArray(arr, arrSize);
     return 0;
 }
+
+
+
+
